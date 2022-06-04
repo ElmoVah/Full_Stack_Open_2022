393070cf20c75cf2189cd62f1ea59ab2a8f2be86
Elmo V <72126010+ElmoVah@users.noreply.github.com>
2022-06-04 12:22:05 +0300

    Exercise 1.14 completed

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -8,6 +8,14 @@ const Button = ({handleClick, text}) => {
   )
 }
 
+const VotesLine = ({votes, selected}) => {
+  return(
+    <>
+      Has {votes[selected]} votes.
+    </>
+  )
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often',
@@ -21,28 +29,47 @@ const App = () => {
    
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Uint8Array(7))
+  const [mostVotes, setMostVotes] = useState(0)
 
   const handleChange = () => {
     setSelected(Math.floor(Math.random() * 7))
   }
 
   const handleVote = () => {
-    const votesCopy = [...votes]
-    votesCopy[selected] += 1
-    setVotes(votesCopy);
+    const newVotes = [...votes]
+    newVotes[selected] += 1
+
+    if (newVotes[selected] > votes[mostVotes]) {
+      var copySelected = selected;
+      setMostVotes(copySelected);
+    } 
+
+    setVotes(newVotes);
   }
 
   return (
     <div>
       <div>
+        <h1>
+          Anecdote of the day
+        </h1>
         {anecdotes[selected]}
       </div>
       <div>
-        Has {votes[selected]} votes.
+        <VotesLine votes={votes} selected={selected} />
       </div>
       <div>
         <Button handleClick={handleVote} text="Vote" />
         <Button handleClick={handleChange} text="Next Anecdote" />
+      </div>
+      <div>
+        <h1>
+          Anecdote with most votes
+        </h1>
+        {anecdotes[mostVotes]}
+      </div>
+      <div>
+        <VotesLine votes={votes} selected={mostVotes} />
       </div>
     </div>
   )
