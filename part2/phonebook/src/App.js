e9857fc7d7df5674f767df57cda60d38c131dfec
Elmo V <72126010+ElmoVah@users.noreply.github.com>
2022-06-06 14:22:45 +0300

    Exercise 2.8 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,9 +3,12 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1234567'
+     }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -13,8 +16,10 @@ const App = () => {
     if (persons.some(person => person.name === newName)) {
       window.alert(`${newName} is already added to phonebook`)
     } else {
+      
       const personObject = {
         name: newName,
+        number: newNumber,
       }
 
       setPersons(persons.concat(personObject))
@@ -27,6 +32,11 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const hadlgeNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -36,6 +46,12 @@ const App = () => {
             value={newName}
             onChange={handlePersonChange}
           />
+          <div>
+            number: <input
+              value={newNumber}
+              onChange={hadlgeNumberChange}
+            />
+          </div>
         </div>
         <div>
           <button type="submit">add</button>
@@ -43,7 +59,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map(person =>
-        <Person key={person.name} name={person.name} />
+        <Person key={person.name} person={person} />
       )}
     </div>
   )
