b9d7ac9a90377d647c584491be64c2a3ce919043
Elmo V <72126010+ElmoVah@users.noreply.github.com>
2022-08-20 20:59:55 +0300

    Exercises 8.21 and 8.22 completed
    In the previous two exercises, the filtering could have been done using just React. To complete this exercise, you should redo the filtering the books based on a selected genre (that was done in exercise 8.19) using a GraphQL query to the server. If you already did so then you do not have to do anything.
    
    If you did the previous exercise, that is, fetch the books in a genre with GraphQL, ensure somehow that the books view is kept up to date. So when a new book is added, the books view is updated at least when a genre selection button is pressed.

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,22 +1,44 @@
-import { useQuery } from '@apollo/client'
+import { useQuery, useLazyQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-const Books = (props) => {
-  const result = useQuery(ALL_BOOKS)
+const Books = props => {
+  const allBooks = useQuery(ALL_BOOKS)
+  const [filterBooks, result] = useLazyQuery(ALL_BOOKS, {
+    fetchPolicy: 'network-only'
+  })
   const [genreFilter, setGenreFilter] = useState('all')
+
+  useEffect(()=> {
+    filterBooks()
+  }, []) //eslint-disable-line
 
   if (!props.show) {
     return null
   }
 
-  if (result.loading) {
-    return <div>loading...</div>;
+  if (allBooks.loading || result.loading) {
+    return <div>loading...</div>
   }
 
-  const allBooks = result.data.allBooks
-  const genres = [...new Set(allBooks.flatMap(book => book.genres))]
-  const booksFiltered = (allBooks).filter(book => book.genres.includes(genreFilter))
+  if (allBooks.error || result.error) {
+    return <div>Error!</div>
+  }
+
+  const genres = [...new Set((allBooks?.data?.allBooks).flatMap(book => book.genres))]
+  
+  const books = result?.data?.allBooks
+  ? result?.data?.allBooks
+  : allBooks?.data?.allBook
+
+  const handleClick = genre => {
+    setGenreFilter(genre)
+    if (genre === 'all') {
+      filterBooks()
+    } else {
+      filterBooks({ variables: { genre: genre } })
+    }
+  }
 
   return (
     <div>
@@ -31,7 +53,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {(genreFilter === 'all' ? allBooks : booksFiltered ).map((a) => (
+          {books?.map(a => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -41,10 +63,12 @@ const Books = (props) => {
         </tbody>
       </table>
       <div>
-        {genres.map((g) => (
-          <button onClick={() => setGenreFilter(g)} key={g}>{g}</button>
+        {genres?.map(g => (
+          <button onClick={() => handleClick(g)} key={g}>
+            {g}
+          </button>
         ))}
-        <button onClick={() => setGenreFilter('all')}>all</button>
+        <button onClick={() => handleClick('all')}>all</button>
       </div>
     </div>
   )
